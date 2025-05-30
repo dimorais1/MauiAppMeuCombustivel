0003c1a0a8cd229b46098670d16ce1388579d73c
dimorais <bruno.araujo38@fatec.sp.gov.br>
2025-05-29 21:03:43 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombistivel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMeuCombistivel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppMeuCombistivel/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace MauiAppMeuCombistivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+                    double etanol = Convert.ToDouble(txt_etanol.Text);
+                    double gasolina = Convert.ToDouble(txt_gasolina.Text);
+                    string msg = "";
+                    string marca = txt_marca.Text;
+                    string modelo = txt_modelo.Text;
+                    if (etanol <= (gasolina * 0.7))
+                    {
+                        msg = $"O etanol está compensando para seu {marca} {modelo}.";
+                    }
+                    else
+                    {
+                        msg = $"A gasolina está compensando para seu {marca} {modelo}.";
+                    }
+                    DisplayAlert("Calculado", msg, "OK");
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Ops", ex.Message, "Fechar");
+                }
+            } // Fecha método
+        } //Fecha Class
+    } // Fecha namespace
+}
